9779e7eac8ddc535a920bcd0873ef424e34c94dd
C-mon <61929326+Cmon-Aa@users.noreply.github.com>
2021-06-01 22:45:07 +0200

    Analog sensor added

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Cmon-Aa/Sesnrortest/internal/iota"
+	"log"
+	"time"
+
+	"github.com/shanghuiyang/rpi-devices/dev"
+	"github.com/stianeikeland/go-rpio"
+)
+
+const (
+	pinTrig = 21
+	pinEcho = 26
+)
+
+func main() {
+
+	// see this for generating a seed: https://docs.iota.org/docs/getting-started/1.1/transfer-tokens/create-a-seed
+	iota, err := iota.WithSeed("DCUFIZDXDODUSKAOFKWQGZEWMJQJ9LI9YOPZGYYPKBZERPUMRVLWLXCDUHVUADV9CWBKLLABJLVAKFODR")
+	if err != nil {
+		panic(err)
+	}
+
+
+	if err := rpio.Open(); err != nil {
+		log.Fatalf("failed to open rpio, error: %v", err)
+		return
+	}
+	defer rpio.Close()
+
+	hcsr04 := dev.NewHCSR04(pinTrig, pinEcho)
+	for {
+		dist := hcsr04.Dist()
+		fmt.Printf("%.2f cm\n", dist)
+		time.Sleep(1 * time.Second)
+
+	tx,_ := iota.SendToTangle(fmt.Sprintf("%.2f cm\n", dist ))
+	if err != nil {
+		panic(err)	
+	}
+	fmt.Printf("tangle transaction: %v\n", tx)
+	// see the transaction on https://explorer.iota.org/legacy-devnet
+}
+}
